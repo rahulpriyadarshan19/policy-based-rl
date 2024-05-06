1a3415c0701fb2f278f9777beb00db0764af49d9
Rahul Priyadarshan <recaptcha19@alumni.iitm.ac.in>
2024-05-06 09:28:31 +0200

    Added final runs

--- reinforce.py
+++ reinforce.py
@@ -221,12 +221,16 @@ class Policy_based_RL():
 
                 # Convert NumPy arrays to torch Tensors to allow for vectorized operations
                 states = torch.stack(states)
+                # states = states.clone().detach()
                 
                 value_estimate_per_state = self.calculate_value_estimate(states, rewards)
 
                 # Convert NumPy arrays to torch Tensors to allow for vectorized operations
                 actions = torch.LongTensor(actions)
                 value_estimate_per_state = torch.Tensor(value_estimate_per_state)
+
+                # actions = actions.clone().detach()
+                # value_estimate_per_state = value_estimate_per_state.clone().detach()
 
                 # Calculate loss of policy network
                 policies = self.policy(states[:-1]) # pi(s)
@@ -239,31 +243,28 @@ class Policy_based_RL():
                     # Calculate loss of value function network
 
                     if self.method == "bootstrapping":
-                        value_function_loss_per_trace.append(torch.mean(torch.square(value_estimate_per_state - self.value_function(states[:-1]).squeeze())) + self.eta_entropy*(torch.sum(-selected_policies*log_policies)))
-                    
+                        # value_function_loss_per_trace.append(torch.mean(torch.square(value_estimate_per_state.detach() - self.value_function(states[:-1]).squeeze())))
+                        value_function_loss_per_trace.append(torch.mean(torch.square(value_estimate_per_state.detach() - self.value_function(states[:-1]).squeeze().detach())))
                     else:
-                        value_function_loss_per_trace.append(torch.mean(torch.square(value_estimate_per_state)) + self.eta_entropy*(torch.sum(-selected_policies*log_policies)))
+                        value_function_loss_per_trace.append(torch.mean(torch.square(value_estimate_per_state.detach())))
 
             # Convert NumPy array to torch Tensors to update the policy's parameters
             policy_loss_per_trace = torch.stack(policy_loss_per_trace)
             mean_policy_loss_per_batch = -torch.mean(policy_loss_per_trace)
 
-            if self.method == "reinforce":
-                self.optimizer_policy.zero_grad()
-                mean_policy_loss_per_batch.backward()
-                self.optimizer_policy.step()
+            self.optimizer_policy.zero_grad()
+            mean_policy_loss_per_batch.backward()
+            self.optimizer_policy.step()
 
-            else:
+            if self.method != "reinforce":
                 value_function_loss_per_trace = torch.stack(value_function_loss_per_trace)
                 mean_value_function_loss_per_batch = -torch.mean(value_function_loss_per_trace)
 
-                self.optimizer_policy.zero_grad()
                 self.optimizer_value_function.zero_grad()
 
-                loss_per_batch = mean_policy_loss_per_batch + mean_value_function_loss_per_batch
-                loss_per_batch.backward()
+                mean_value_function_loss_per_batch.requires_grad = True
+                mean_value_function_loss_per_batch.backward()
 
-                self.optimizer_policy.step()
                 self.optimizer_value_function.step()
 
             if time % int(eval_interval / (n_traces * self.trace_length)):
@@ -283,7 +284,8 @@ class Policy_based_RL():
 
         return  eval_timesteps, eval_mean_reward
     
-def averaged_runs(hyperparam, hyperparam_array, hyperparam_label, method = "reinforce"):
+def averaged_runs(hyperparam, hyperparam_array, hyperparam_label, method = "reinforce",
+                  best = False):
     """Function to generate results averaging over 20 runs and for hyperparameter optimization.
     hyperparam: Hyperparameter to be optimized (string)
     hyperparam_array: Array of possible hyperparameters
@@ -293,55 +295,101 @@ def averaged_runs(hyperparam, hyperparam_array, hyperparam_label, method = "rein
     fig, ax = plt.subplots(nrows = 1, ncols = 1)
     color_strings = ["blue", "green", "red"]
     
-    for value, color_string in zip(hyperparam_array, color_strings):
+    if not best:
+        for value, color_string in zip(hyperparam_array, color_strings):
+            
+            mean_eval_timesteps = [] # Averaging mean environment steps over many runs
+            mean_eval_rewards = [] # Averaging mean reward over many runs
+            lengths = []    # Length of each run
+            print(f"{hyperparam} = {value}: ")
+            
+            for i in tqdm(range(5)):
+                
+                # Making the environment
+                env = gym.make("Acrobot-v1")
+                eval_env = gym.make("Acrobot-v1")
+            
+                # Defining the policy based RL class based on which hyperparameter needs to be tuned
+                if hyperparam == "learning_rate":
+                    policy_based_init = Policy_based_RL(
+                        env, eval_env, method = "reinforce", trace_length = 200,
+                        discount = 0.99, n_bootstrapping = 3, learning_rate = value, 
+                        eta_entropy = 0.01, n_nodes = 32, n_hidden_layers = 1)
+                    
+                elif hyperparam == "n_nodes":
+                    policy_based_init = Policy_based_RL(
+                        env, eval_env, method = "reinforce", trace_length = 200,
+                        discount = 0.99, n_bootstrapping = 3, learning_rate = 0.005, 
+                        eta_entropy = 0.01, n_nodes = value, n_hidden_layers = 1)
+                    
+                elif hyperparam == "n_hidden_layers":
+                    policy_based_init = Policy_based_RL(
+                        env, eval_env, method = "reinforce", trace_length = 200,
+                        discount = 0.99, n_bootstrapping = 3, learning_rate = 0.005, 
+                        eta_entropy = 0.01, n_nodes = 32, n_hidden_layers = value)
+                    
+                elif hyperparam == "n_bootstrapping":
+                    policy_based_init = Policy_based_RL(
+                        env, eval_env, method = "bootstrapping", trace_length = 200,
+                        discount = 0.99, n_bootstrapping = value, learning_rate = 0.005, 
+                        eta_entropy = 0.03, n_nodes = 32, n_hidden_layers = 1)
+                    
+                elif hyperparam == "eta_entropy":
+                    policy_based_init = Policy_based_RL(
+                        env, eval_env, method = "reinforce", trace_length = 200,
+                        discount = 0.99, n_bootstrapping = 3, learning_rate = 0.005, 
+                        eta_entropy = value, n_nodes = 32, n_hidden_layers = 1)
+                    
+                # Training the policy-based algorithm and evaluating it on an environment
+                eval_timesteps, eval_mean_reward = np.array(policy_based_init.general_policy_based_algorithm
+                                                            (n_timesteps = 803000, 
+                                                            n_traces = 5, 
+                                                            eval_interval = 3000))
+                
+                mean_eval_timesteps.append(eval_timesteps)
+                mean_eval_rewards.append(eval_mean_reward)
+
+                env.close()
+                eval_env.close()
+                
+            # Taking the shortest length of all episodes because each episode is of different length
+            mean_eval_timesteps = np.mean(mean_eval_timesteps, axis = 0)
+            mean_eval_rewards = np.mean(mean_eval_rewards, axis = 0)
+            
+            # Smoothing the rewards
+            mean_eval_rewards = savgol_filter(mean_eval_rewards, window_length = 5,
+                                            polyorder = 2)
+            
+            # Plotting learning curves
+            ax.grid()
+            ax.plot(mean_eval_timesteps, mean_eval_rewards, label = f"{hyperparam_label} = {value}",
+                    color = colors[color_string])
+            
+        ax.set_xlabel("Number of environment steps")
+        ax.set_ylabel("Mean reward of evaluation environment")
+        ax.set_title(f"Tuning {hyperparam}")
+        ax.legend()
+        fig.savefig(f"learning_curve_{method}_{hyperparam}.png", dpi = 300)
         
+    elif best:
         mean_eval_timesteps = [] # Averaging mean environment steps over many runs
         mean_eval_rewards = [] # Averaging mean reward over many runs
-        lengths = []    # Length of each run
-        print(f"{hyperparam} = {value}: ")
         
         for i in tqdm(range(20)):
-            
             # Making the environment
             env = gym.make("Acrobot-v1")
             eval_env = gym.make("Acrobot-v1")
-        
-            # Defining the policy based RL class based on which hyperparameter needs to be tuned
-            if hyperparam == "learning_rate":
-                policy_based_init = Policy_based_RL(
-                    env, eval_env, method = "reinforce", trace_length = 200,
-                    discount = 0.99, n_bootstrapping = 3, learning_rate = value, 
-                    eta_entropy = 0.01, n_nodes = 32, n_hidden_layers = 1)
-                
-            elif hyperparam == "n_nodes":
-                policy_based_init = Policy_based_RL(
-                    env, eval_env, method = "reinforce", trace_length = 200,
-                    discount = 0.99, n_bootstrapping = 3, learning_rate = 0.05, 
-                    eta_entropy = 0.01, n_nodes = value, n_hidden_layers = 1)
-                
-            elif hyperparam == "n_hidden_layers":
-                policy_based_init = Policy_based_RL(
-                    env, eval_env, method = "reinforce", trace_length = 200,
-                    discount = 0.99, n_bootstrapping = 3, learning_rate = 0.05, 
-                    eta_entropy = 0.01, n_nodes = 64, n_hidden_layers = value)
-                
-            elif hyperparam == "n_bootstrapping":
-                policy_based_init = Policy_based_RL(
-                    env, eval_env, method = "bootstrapping", trace_length = 200,
-                    discount = 0.99, n_bootstrapping = value, learning_rate = 0.05, 
-                    eta_entropy = 0.01, n_nodes = 64, n_hidden_layers = 1)
-                
-            elif hyperparam == "eta_entropy":
-                policy_based_init = Policy_based_RL(
-                    env, eval_env, method = "reinforce", trace_length = 200,
-                    discount = 0.99, n_bootstrapping = 3, learning_rate = 0.05, 
-                    eta_entropy = value, n_nodes = 64, n_hidden_layers = 1)
-                
+            
+            policy_based_init = Policy_based_RL(
+                env, eval_env, method = method, trace_length = 200,
+                discount = 0.99, n_bootstrapping = 3, learning_rate = 0.005, 
+                eta_entropy = 0.03, n_nodes = 32, n_hidden_layers = 1)
+            
             # Training the policy-based algorithm and evaluating it on an environment
             eval_timesteps, eval_mean_reward = np.array(policy_based_init.general_policy_based_algorithm
-                                                        (n_timesteps = 51000, 
-                                                         n_traces = 5, 
-                                                         eval_interval = 3000))
+                                                        (n_timesteps = 803000, 
+                                                        n_traces = 5, 
+                                                        eval_interval = 3000))
             
             mean_eval_timesteps.append(eval_timesteps)
             mean_eval_rewards.append(eval_mean_reward)
@@ -354,19 +402,15 @@ def averaged_runs(hyperparam, hyperparam_array, hyperparam_label, method = "rein
         mean_eval_rewards = np.mean(mean_eval_rewards, axis = 0)
         
         # Smoothing the rewards
-        mean_eval_rewards = savgol_filter(mean_eval_rewards, window_length = 9,
-                                          polyorder = 2)
+        mean_eval_rewards = savgol_filter(mean_eval_rewards, window_length = 5,
+                                        polyorder = 2)
         
-        # Plotting learning curves
         ax.grid()
-        ax.plot(mean_eval_timesteps, mean_eval_rewards, label = f"{hyperparam_label} = {value}",
-                color = colors[color_string])
-        
-    ax.set_xlabel("Number of environment steps")
-    ax.set_ylabel("Mean reward of evaluation environment")
-    ax.set_title(f"Tuning {hyperparam}")
-    ax.legend()
-    fig.savefig(f"learning_curve_{method}_{hyperparam}.png", dpi = 300)
+        ax.plot(mean_eval_timesteps, mean_eval_rewards, color = colors["blue"])
+        ax.set_xlabel("Number of environment steps")
+        ax.set_ylabel("Mean reward of evaluation environment")
+        ax.set_title(f"Performance of {method}")
+        fig.savefig(f"performance_{method}.png", dpi = 300)
             
     
 if __name__ in "__main__":
@@ -378,12 +422,20 @@ if __name__ in "__main__":
         "n_bootstrapping": [np.array([1, 3, 5], dtype = int), "n-step"],
         "eta_entropy": [np.array([0.003, 0.01, 0.03], dtype = np.float32), r"$\eta$"]
     }
-    print(list(hyperparam_dict.keys()))
-    for hyperparam in list(hyperparam_dict.keys())[4:5]:
-        averaged_runs(hyperparam, 
-                      hyperparam_array = hyperparam_dict[hyperparam][0], 
-                      hyperparam_label = hyperparam_dict[hyperparam][1], 
-                      method = "reinforce")
+    # print(list(hyperparam_dict.keys()))
+    # for hyperparam in list(hyperparam_dict.keys())[3:4]:
+    #     averaged_runs(hyperparam, 
+    #                   hyperparam_array = hyperparam_dict[hyperparam][0], 
+    #                   hyperparam_label = hyperparam_dict[hyperparam][1], 
+    #                   method = "reinforce")
         
+    # Getting the best runs of bootstrapping and bootstrapping_baseline_subtraction
+    hyperparam = list(hyperparam_dict.keys())[0]
+    averaged_runs(hyperparam, hyperparam_dict[hyperparam][0], hyperparam_dict[hyperparam][1],
+                  method = "bootstrapping", best = True)
+    averaged_runs(hyperparam, hyperparam_dict[hyperparam][0], hyperparam_dict[hyperparam][1],
+                  method = "bootstrapping_baseline_subtraction", best = True)
+    
+    
         
  
